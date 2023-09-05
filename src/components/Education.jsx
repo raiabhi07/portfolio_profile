37d390fb877d2df87bab447405402dc507604bc4
Abhishek Rai <90126255+raiabhi07@users.noreply.github.com>
2023-09-05 09:53:06 +0530

    Add files via upload

---
+++ src/components/Education.jsx
@@ -0,0 +1,21 @@
+
+export const education = [
+    {
+        Institute: "Gatutam Buddha University",
+        Degree: "Bachelor of Technology in Computer Science & Engineering",
+        Year: "2020 - 2024",
+        Marks: "8.1 CGPA"
+    },
+    {
+        Institute: "Sunbeam School Ghazipur",
+        Degree: "12th CBSE PCM",
+        Year: "2018 - 2020",
+        Marks: "83%"
+    },
+    {
+        Institute: "Sunbeam School Ghazipur",
+        Degree: "10th CBSE",
+        Year: "2016 - 2018",
+        Marks: "89.66%"
+    }
+];
---
+++ src/components/Experience.jsx
@@ -0,0 +1,11 @@
+import logo1 from '../Images/cw.jfif'
+export const experience = [
+    {
+        img: logo1,
+        header: "Code Watchers",
+        subHeader: "Full stack developer Intern",
+        subTitleHeader: "Arp 2023 - Present",
+        para1: "My role is to do authentication based on JWT tockens",
+        para2: "Made partial page CRUD operations",
+    }
+];
